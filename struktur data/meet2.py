a38871cb603ee175fcf00c5d6fb109405cb81127
azzam1342 <146051837+azzam1342@users.noreply.github.com>
2024-04-01 12:05:24 +0800

    meet2.py senin 1-maret-2024

---
+++ struktur data/meet2.py
@@ -0,0 +1,20 @@
+# belajar variable
+# assigment
+x= 3
+y= 6
+uang = 1000
+
+# pemanggilan
+print ("Nilai UAS saya =", x)
+print ("Nilai UTS saya =", y)
+print ("sisa uang saya =", uang)
+
+# penamaan variable
+nilai_a = 9
+kotak10 = 10
+
+# input variable kedua
+x = 30
+print ("Nilai UAS saya =", x)
+x = 300
+print ("Nilai UAS saya =", x)
