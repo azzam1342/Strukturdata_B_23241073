85d7f8c4bb7e9b0e57b87a8e87bc47da1c120234
azzam1342 <146051837+azzam1342@users.noreply.github.com>
2024-04-22 11:53:12 +0800

    senin 22-april-2024

---
+++ exe3.py
@@ -0,0 +1,21 @@
+# contoh tipe data (integer) : angka satuan
+bil_integer = 1
+print(type(bil_integer))
+print ("type data dari bil_integer adalah: ", type(bil_integer))
+
+#contoh tipe data (float) : bilangan desimal
+bil_float = 2.5
+print ("type data bil_float adalah: ", type (bil_float))
+
+# contoh tipe data (string) : kumpulan karakter
+bil_string = "putu"
+print ("type data bil_float adalah: ", type (bil_string))
+
+# contoh tipe data (bolean) : biner, 0,1, True Flase
+bil_bol = True
+print ("type data bil_float adalah: ", type (bil_bol))
+
+# tipe data dari bahasa C
+from ctypes import c_double
+bil_double = c_double(2000000)
+print ("type data bil_double adalah: ", type (bil_double))
