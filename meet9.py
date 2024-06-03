87aac5535ae095217cafc651fbec4beaa7f7de82
azzam1342 <146051837+azzam1342@users.noreply.github.com>
2024-06-03 11:49:21 +0800

    Add files via upload

---
+++ meet9.py
@@ -0,0 +1,24 @@
+# percabangan jika maka
+
+name = input("what is your name : ")
+
+# if kondisi : aksi 
+
+# 1. if standar
+# if name == "azzam" : print("Mahasiswa ganteng")
+# print("end of program")
+
+
+# 2. if indetansi
+if name =="azzam":
+    print("Mahasiswa ganteng")
+    print("ganteng banget")
+print("end of program")
+
+# 3. if ELSE
+if name=="azzam":
+    print("Mahasiswa ganteng")
+    print(f"terimakasih {name}")
+else:
+    print("anda tidak ganteng")
+print("end of program")
